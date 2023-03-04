27d0dbf33615ac818885be3ceaca644cdbe6fbc6
Jakob <pipejakob@gmail.com>
2023-03-04 15:11:01 -0800

    Allow specifying a directory other than $PWD.

--- cmd/godot_web/main.go
+++ cmd/godot_web/main.go
@@ -13,8 +13,17 @@ var allowExternal = flag.Bool("external", false, "allow external traffic")
 var tlsCert = flag.String("tls-cert", "", "path to certificate file (in PEM format)")
 var tlsKey = flag.String("tls-key", "", "path to private key file (in PEM format)")
 var showVersion = flag.Bool("version", false, "print the current version and exit")
+var dir string // the last argument (optional)
+
+func usage() {
+	fmt.Fprintln(flag.CommandLine.Output(), "Usage: godot_web [OPTIONS] [DIR]\n\n"+
+		"If DIR is omitted, the current working directory is used.\n\n"+
+		"Options:")
+	flag.PrintDefaults()
+}
 
 func main() {
+	flag.Usage = usage
 	flag.Parse()
 
 	validateFlags()
@@ -24,11 +33,6 @@ func main() {
 		return
 	}
 
-	dir, err := os.Getwd()
-	if err != nil {
-		panic(fmt.Errorf("error getting working directory: %v", err))
-	}
-
 	server := godot_web.New(dir, *port, *allowExternal, *tlsCert, *tlsKey)
 	if err := server.Run(); err != nil {
 		panic(fmt.Errorf("error running server: %v", err))
@@ -36,28 +40,60 @@ func main() {
 }
 
 func validateFlags() {
-	if flag.NArg() != 0 {
-		fmt.Printf("Unexpected argument(s): %q\n", flag.Args())
+	out := flag.CommandLine.Output()
+
+	if flag.NArg() > 1 {
+		fmt.Fprintf(out, "Too many trailing arguments: %q\n", flag.Args())
 		usage()
 		os.Exit(1)
 	}
 
+	if flag.NArg() == 1 {
+		dir = flag.Arg(0)
+
+		if err := verifyDir(dir); err != nil {
+			fmt.Fprintf(out, "Problem with directory %q: %v\n", dir, err)
+			usage()
+			os.Exit(1)
+		}
+	} else {
+		var err error
+		if dir, err = os.Getwd(); err != nil {
+			panic(fmt.Errorf("error getting working directory: %v", err))
+		}
+	}
+
 	if *tlsCert != "" || *tlsKey != "" {
 		if *tlsCert == "" || *tlsKey == "" {
-			fmt.Println("If you specify one of --tls-cert or --tls-key, you must specify them both")
+			fmt.Fprintln(out, "If you specify one of --tls-cert or --tls-key, you must specify them both.")
 			usage()
 			os.Exit(1)
 		}
 
 		if !*allowExternal {
-			fmt.Println("The flags --tls-cert and --tls-key can only be used when --external is also given")
+			fmt.Fprintln(out, "The flags --tls-cert and --tls-key can only be used when --external is also given.")
 			usage()
 			os.Exit(1)
 		}
 	}
 }
 
-func usage() {
-	fmt.Println("Usage: godot_web [OPTIONS]")
-	flag.PrintDefaults()
+func verifyDir(path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	info, err := f.Stat()
+	if err != nil {
+		return err
+	}
+
+	if !info.IsDir() {
+		return fmt.Errorf("not a directory")
+	}
+
+	return nil
 }
